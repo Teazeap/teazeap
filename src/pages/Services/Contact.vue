5b0b615939a92e8f4514541644f6b68b3f4c2161
Muzi-P <phiwokuhlegondwe@gmail.com>
2020-11-25 22:27:32 +0800

    doc

--- src/pages/Services/Contact.vue
+++ src/pages/Services/Contact.vue
@@ -4,55 +4,74 @@
         <h2 class="title">Want to work with us?</h2>
         <p class="description">Your teaching career is very important to us.</p>
         <div class="row">
-              <div class="col-lg-5 col-sm-12 text-center ml-auto mr-auto col-md-10">
-                <form name="homepage" method="POST" data-netlify="true" netlify>
-                    <span v-if="false">
-                      <p>
-                        <label>Your Name: <input v-model="form.fullName" type="text" name="name" /></label>   
-                      </p>
-                      <p>
-                        <label>Your Email: <input v-model="form.email" type="email" name="email" /></label>
-                      </p>
-                      <p>
-                        <label>Message: <textarea v-model="form.message" name="message"></textarea></label>
-                      </p>
-                    </span>
-                  
-                    <fg-input
-                      class="input-lg"
-                      placeholder="First Name..."
-                      v-model="form.fullName"
-                      type="text"
-                      addon-left-icon="now-ui-icons users_circle-08"
-                      name="Full Name"
-                    >
-                    </fg-input>
-                    <fg-input
-                      class="input-lg"
-                      placeholder="Email Here..."
-                      type="email"
-                      v-model="form.email"
-                      addon-left-icon="now-ui-icons ui-1_email-85"
-                    >
-                    </fg-input>
-                    <div class="textarea-container">
-                      <textarea
-                        class="form-control"
-                        name="name"
-                        rows="4"
-                        cols="80"
-                        v-model="form.message"
-                        placeholder="Type a message..."
-                      ></textarea>
-                    </div>
-                    <div class="send-button">
-                      <button type="submit" class="btn-round btn btn-primary btn-lg" rounded >Send Message</button>
-                      <n-button v-if="false" type="submit" round   class="btn btn-primary btn-lg"
-                        >Send Message</n-button
-                      >
-                    </div>
-                </form>
-            </div>
+           <form
+              name="ask-question"
+              method="post"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              >
+              <input type="hidden" name="form-name" value="ask-question" />
+              <label v-for="(panelist, index) in panelists" :key="index">
+                <input
+                  type="radio"
+                  name="panelist"
+                  :value="panelist"
+                  @input="ev => updatePanelist"
+                  :checked="panelist === currentPanelist"
+                />
+                <span>{{ panelist }}</span>
+              </label>
+              <button>Submit</button>
+            </form>
+          <!-- <div class="col-lg-5 col-sm-12 text-center ml-auto mr-auto col-md-10">
+            <form name="homepage" method="POST" data-netlify="true" netlify>
+                <span v-if="false">
+                  <p>
+                    <label>Your Name: <input v-model="form.fullName" type="text" name="name" /></label>   
+                  </p>
+                  <p>
+                    <label>Your Email: <input v-model="form.email" type="email" name="email" /></label>
+                  </p>
+                  <p>
+                    <label>Message: <textarea v-model="form.message" name="message"></textarea></label>
+                  </p>
+                </span>
+              
+                <fg-input
+                  class="input-lg"
+                  placeholder="First Name..."
+                  v-model="form.fullName"
+                  type="text"
+                  addon-left-icon="now-ui-icons users_circle-08"
+                  name="Full Name"
+                >
+                </fg-input>
+                <fg-input
+                  class="input-lg"
+                  placeholder="Email Here..."
+                  type="email"
+                  v-model="form.email"
+                  addon-left-icon="now-ui-icons ui-1_email-85"
+                >
+                </fg-input>
+                <div class="textarea-container">
+                  <textarea
+                    class="form-control"
+                    name="name"
+                    rows="4"
+                    cols="80"
+                    v-model="form.message"
+                    placeholder="Type a message..."
+                  ></textarea>
+                </div>
+                <div class="send-button">
+                  <button type="submit" class="btn-round btn btn-primary btn-lg" rounded >Send Message</button>
+                  <n-button v-if="false" type="submit" round   class="btn btn-primary btn-lg"
+                    >Send Message</n-button
+                  >
+                </div>
+            </form>
+          </div> -->
         </div>
       </div>
     </div>
@@ -73,7 +92,9 @@ export default {
           fullName: '',
           email: '',
           message: ''
-        }
+        },
+        panelists: ['Evan You', 'Chris Fritz'],
+        currentPanelist: 'Evan You'
       };
     },
     methods : {
@@ -83,11 +104,14 @@ export default {
         text: 'You will recieve email for confirmation',
         icon: 'success',
         confirmButtonText: 'Okay'
-      }).then((result) => {
+        }).then((result) => {
         if(result.value) {
           this.form = {}
-        }
-      })
+          }
+        })
+      },
+      updatePanelist (ev) {
+        this.currentPanelist = ev.target.value
       }
     }
 }

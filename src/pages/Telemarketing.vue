a523580e1cf032d8dc29e2865480966b5903646e
Muzi Gondwe <muzi.gondwe@fariaedu.com>
2023-05-12 18:53:43 +0900

    add: telemarketing contact form

--- src/pages/Telemarketing.vue
+++ src/pages/Telemarketing.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <Header />
-        <div class="pt-0 intro-container">
-      <div class="container process intro">
-        <h2 class="title  text-center">
+    <div class="pt-0 intro-container">
+      <div class="container section-card intro">
+        <h2 class="title text-center">
           Let us build your virtual telemarketing phone room.
         </h2>
         <div class="row mt-4">
@@ -12,50 +12,58 @@
           </v-col>
           <v-col cols="12" sm="5" lg="6">
             <p class="introduction-text">
-            Welcome to Teazeap, your  trusted telemarketing partner for business success. Our team of experienced and skilled telemarketing professionals are dedicated to providing top-notch services to businesses of all sizes. From generating leads to increasing sales, we help you achieve your marketing goals with the power of telemarketing. Promote your brand, connect with your target audience, and grow your business with Teazeap. Get in touch with us today to see how we can help you take your business to the next level.
-          </p>
+              Welcome to Teazeap, your trusted telemarketing partner for
+              business success. Our team of experienced and skilled
+              telemarketing professionals are dedicated to providing top-notch
+              services to businesses of all sizes. From generating leads to
+              increasing sales, we help you achieve your marketing goals with
+              the power of telemarketing. Promote your brand, connect with your
+              target audience, and grow your business with Teazeap. Get in touch
+              with us today to see how we can help you take your business to the
+              next level.
+            </p>
           </v-col>
         </div>
       </div>
     </div>
-    <!-- <div class="section-about-us intro-container">
-      <div class="container process intro">
-        <h2 class="title  text-center">
-          Let us build your virtual telemarketing phone room.
-        </h2>
-        <p class="video-section-introduction">
-          Welcome to Teazeap, your  trusted telemarketing partner for business success. Our team of experienced and skilled telemarketing professionals are dedicated to providing top-notch services to businesses of all sizes. From generating leads to increasing sales, we help you achieve your marketing goals with the power of telemarketing. Promote your brand, connect with your target audience, and grow your business with Teazeap. Get in touch with us today to see how we can help you take your business to the next level.
-        </p>
-        <div class="separator separator-primary"></div>
-      </div>
-    </div> -->
     <!-- video section -->
     <div class="section pt-0 mobile-padding">
-      <div class="container process">
-        <h2 class="title  text-center">
+      <div class="container section-card">
+        <h2 class="title text-center">
           Get a Glimpse Inside: Watch Our Call Center Agents in Action
         </h2>
         <p class="video-section-introduction">
-          At Teazeap, we take pride in our team of professional and dedicated call center agents who work tirelessly to provide exceptional service to our clients. Our video gallery offers an inside look into the daily operations of our call center, highlighting the professionalism, expertise, and dedication of our agents. See firsthand how our team works to generate leads, increase sales, and promote brands through the power of telemarketing.
+          At Teazeap, we take pride in our team of professional and dedicated
+          call center agents who work tirelessly to provide exceptional service
+          to our clients. Our video gallery offers an inside look into the daily
+          operations of our call center, highlighting the professionalism,
+          expertise, and dedication of our agents. See firsthand how our team
+          works to generate leads, increase sales, and promote brands through
+          the power of telemarketing.
         </p>
 
         <div class="row mt-4 videos-container">
-          <div class="col-md-4" v-for="(video, id) in videos" :key="id">
-            <iframe
-              width="100%"
-              height="345"
-              class="video-frame"
-              :src="video.src"
-            >
-            </iframe>
-          </div>
+          <flickity
+            :options="flickityOptions"
+            class="video-carousel"
+          >
+            <div class="col-md-4" v-for="(video, id) in videos" :key="id">
+              <iframe
+                width="100%"
+                height="345"
+                class="video-frame"
+                :src="video.src"
+              >
+              </iframe>
+            </div>
+          </flickity>
         </div>
       </div>
     </div>
     <!-- video section -->
     <div class="section pt-0 mobile-padding">
-      <div class="container process">
-        <h2 class="title  text-center">
+      <div class="container section-card">
+        <h2 class="title text-center">
           Our Strategic Approach To Providing Telemarketing Services
         </h2>
         <div class="row mt-4">
@@ -71,10 +79,13 @@
 
     <!-- plans section -->
     <div class="section pt-0 mobile-padding">
-      <div class="container process">
+      <div class="container section-card">
         <h2 class="title">CHOOSING Your Solution:</h2>
         <h5 class="description">
-          If you're looking for a reliable partner to help you reach your sales goals and improve customer engagement, look no further than Teazeap. Our telemarketing and call center services are tailored to meet your unique needs and deliver results.
+          If you're looking for a reliable partner to help you reach your sales
+          goals and improve customer engagement, look no further than Teazeap.
+          Our telemarketing and call center services are tailored to meet your
+          unique needs and deliver results.
         </h5>
         <div class="row mt-n4 mb-4" style="justify-content: center">
           <v-col cols="12" sm="7" lg="4">
@@ -82,7 +93,7 @@
               :card-styles="{
                 backgroundColor: 'rgb(96, 125, 249)',
                 actionButtonColor: 'rgb(96, 125, 249)',
-                actionButtonBackground: 'white'
+                actionButtonBackground: 'white',
               }"
               :plan="{
                 title: 'AVATAR',
@@ -90,12 +101,12 @@
                 keyPoints: [
                   'Identify ideal customers for products/services.',
                   'Research potential leads for sales.',
-                  'Generate leads through surveys.'
+                  'Generate leads through surveys.',
                 ],
                 actionButton: {
                   text: 'Reserve',
-                  url: ''
-                }
+                  url: '',
+                },
               }"
             />
           </v-col>
@@ -105,7 +116,7 @@
                 backgroundColor: '#f96332',
                 textColor: 'white',
                 actionButtonColor: '#f96332',
-                actionButtonBackground: 'white'
+                actionButtonBackground: 'white',
               }"
               :plan="{
                 title: 'SPECIALS',
@@ -113,12 +124,12 @@
                 keyPoints: [
                   'Credit check for prospects.',
                   'Thorough research for sales leads.',
-                  'Maintain accurate CRM data.'
+                  'Maintain accurate CRM data.',
                 ],
                 actionButton: {
                   text: 'Reserve',
-                  url: ''
-                }
+                  url: '',
+                },
               }"
             />
           </v-col>
@@ -127,8 +138,8 @@
     </div>
     <!-- testimonials section -->
     <div class="section pt-0 mobile-padding">
-      <div class="container process">
-        <h2 class="title  text-center">Real Clients. Real Results</h2>
+      <div class="container section-card">
+        <h2 class="title text-center">Real Clients. Real Results</h2>
         <flickity
           ref="communityBadgeInfoModalSwiper"
           :options="flickityOptions"
@@ -147,7 +158,7 @@
 </template>
 <script>
 import { Button, FormGroupInput, Collapsible } from "@/components";
-import Contact from "@/pages/Services/Contact";
+import Contact from "@/pages/Telemarketing/Contact";
 import Header from "@/pages/Telemarketing/Header.vue";
 import Flickity from "vue-flickity";
 import TesimonialsCard from "@/pages/Telemarketing/TesimonialsCard";
@@ -164,14 +175,14 @@ export default {
     Header,
     Collapsible,
     Flickity,
-    PlanCard
+    PlanCard,
   },
   data() {
     return {
       form: {
         firstName: "",
         email: "",
-        message: ""
+        message: "",
       },
       team: [
         {
@@ -179,39 +190,36 @@ export default {
           quote: "landing-page.chief-quote",
           pictureUrl: "img/ceo.jpg",
           title: "landing-page.chief",
-          position: "top"
+          position: "top",
         },
         {
           name: "Calerb Louis Jean",
           quote: "landing-page.project-quote",
           pictureUrl: "img/jake_2.jpg",
           title: "landing-page.project",
-          position: "top"
+          position: "top",
         },
         {
           name: "Nnenia Stephens",
           quote: "landing-page.comm-quote",
           pictureUrl: "img/nnenia_1.jpg",
           title: "landing-page.comm",
-          position: "top"
-        }
+          position: "top",
+        },
       ],
       videos: [
         {
           id: 1,
-          src:
-            "https://videos.ctfassets.net/dr15y1pi2yc9/77uBczZTTHGFOYJNjgL6qD/1a4577bff54f4a117ee37a4c7aee7b92/videoplayback-1.mp4"
+          src: "https://videos.ctfassets.net/dr15y1pi2yc9/77uBczZTTHGFOYJNjgL6qD/1a4577bff54f4a117ee37a4c7aee7b92/videoplayback-1.mp4",
         },
         {
           id: 2,
-          src:
-            "https://videos.ctfassets.net/dr15y1pi2yc9/3p9fUqGM6nLVbKWMBSsqt9/634faaa8c15656d5b6ccd0b0d7c8ac1c/videoplayback-2.mp4"
+          src: "https://videos.ctfassets.net/dr15y1pi2yc9/3p9fUqGM6nLVbKWMBSsqt9/634faaa8c15656d5b6ccd0b0d7c8ac1c/videoplayback-2.mp4",
         },
         {
           id: 3,
-          src:
-            "https://videos.ctfassets.net/dr15y1pi2yc9/NVi7i40iXbsu30guZRUak/ce71561a78a2a18bb474181d98437cdc/videoplayback-3.mp4"
-        }
+          src: "https://videos.ctfassets.net/dr15y1pi2yc9/NVi7i40iXbsu30guZRUak/ce71561a78a2a18bb474181d98437cdc/videoplayback-3.mp4",
+        },
       ],
       flickityOptions: {
         adaptiveHeight: false,
@@ -225,25 +233,25 @@ export default {
         autoPlay: 3000,
         wrapAround: true,
         pauseAutoPlayOnHover: false,
-        resize: true
+        resize: true,
       },
       testimonials: [
         {
           picUrl: "img/agent-1.jpg",
           name: "Debt Settlement",
-          message: `"How many other outbound call centers did we try?At least 12 in India and the Philippines. They all failed because of the accents. Avatar & Echo telemarketing outsourcing works, the agents don't have accents and prospects listen to what they say."`
+          message: `"How many other outbound call centers did we try?At least 12 in India and the Philippines. They all failed because of the accents. Avatar & Echo telemarketing outsourcing works, the agents don't have accents and prospects listen to what they say."`,
         },
         {
           picUrl: "img/agent-2.jpg",
           name: "Mortgage",
-          message: `"American-run, they get lead generation. Avatar & Echo delivers the lowest cost, qualified leads. Its as east to get as siding up for their service. Listen to what they say – they know the business."`
+          message: `"American-run, they get lead generation. Avatar & Echo delivers the lowest cost, qualified leads. Its as east to get as siding up for their service. Listen to what they say – they know the business."`,
         },
         {
           picUrl: "img/agent-3.jpg",
           name: "Insurance",
-          message: `With the RXR we manager our team and it feels like we are in the middle of the action.  The software is amazing are we run better then we did when we had the call center here. It's amazing that they turned the complex process of running a call center into a service!`
-        }
-      ]
+          message: `With the RXR we manager our team and it feels like we are in the middle of the action.  The software is amazing are we run better then we did when we had the call center here. It's amazing that they turned the complex process of running a call center into a service!`,
+        },
+      ],
     };
   },
   created() {
@@ -252,7 +260,7 @@ export default {
   computed: {
     swiper() {
       return this.$refs.communityBadgeInfoModalSwiper;
-    }
+    },
   },
   methods: {
     handleLangauge() {
@@ -264,8 +272,8 @@ export default {
       } else {
         this.$i18n.locale = browserLanguage.includes("zh") ? "zh" : "en";
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -282,14 +290,6 @@ export default {
   cursor: pointer;
 }
 
-.process {
-  border: 1px solid #ebeef5;
-  -webkit-box-shadow: 0px 5px 25px 0px rgb(0 0 0 / 20%);
-  box-shadow: 0px 5px 25px 0px rgb(0 0 0 / 20%);
-  transition: 0.3s;
-  border-radius: 24px;
-  margin-top: 4rem;
-}
 .carousel {
   margin: 84px 0 0;
   padding: 0;
@@ -302,6 +302,13 @@ export default {
   margin: 0 20px 0;
   padding: 0;
   width: 300px;
+}
+
+.video-carousel {
+  margin: 84px 0 0;
+  padding: 0;
+  width: 100%;
+  height: 400px;
 }
 
 .video-section-introduction {
@@ -343,10 +350,7 @@ export default {
 
 @media (min-width: 600px) {
   .introduction-text {
-    margin-right: 16px
+    margin-right: 16px;
   }
 }
-
-
-
 </style>
---
+++ src/pages/Telemarketing/Contact.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="contact-section text-center">
+    <div class="container section-card">
+      <h2 class="title">{{ $t("contact.get-in-touch") }}</h2>
+      <p class="description">{{ $t("contact.reach-out") }}</p>
+      <div class="row">
+        <div class="col-lg-5 col-sm-12 text-center ml-auto mr-auto col-md-10">
+          <form
+            @submit.prevent="handleSubmit"
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            netlify-honeybot="bot-field"
+            class="needs-validation"
+            novalidate
+          >
+            <input type="hidden" name="bot-field" value="contact" />
+            <fg-input
+              :class="`input-lg ${errors.has('name') ? 'has-danger' : ''}`"
+              :placeholder="[[firstPlaceholder]]"
+              v-model="form.name"
+              type="text"
+              v-validate="'required'"
+              addon-left-icon="now-ui-icons users_circle-08"
+              name="name"
+            >
+            </fg-input>
+            <span v-show="errors.has('name')" class="text-danger">{{
+              errors.first("name")
+            }}</span>
+            <fg-input
+              :class="`input-lg ${errors.has('name') ? 'has-danger' : ''}`"
+              :placeholder="[[emailPlaceholder]]"
+              type="email"
+              v-validate="'required|email'"
+              v-model="form.email"
+              addon-left-icon="now-ui-icons ui-1_email-85"
+              name="email"
+            >
+            </fg-input>
+            <span v-show="errors.has('email')" class="text-danger">{{
+              errors.first("email")
+            }}</span>
+            <div class="textarea-container">
+              <textarea
+                class="form-control"
+                :class="`${errors.has('name') ? 'has-danger' : ''}`"
+                rows="4"
+                cols="80"
+                v-model="form.message"
+                v-validate="'required'"
+                name="message"
+                :placeholder="[[sendPlaceholder]]"
+              ></textarea>
+              <span v-show="errors.has('message')" class="text-danger">{{
+                errors.first("message")
+              }}</span>
+            </div>
+            <div class="send-button">
+              <button class="btn-round btn btn-primary btn-lg" rounded>
+                {{ $t("contact.send") }}
+              </button>
+            </div>
+            <!-- added for netlify form submissions, not visible in ui -->
+            <div style="position: absolute;z-index: -1;top: 0; opacity: 0">
+              <fg-input v-model="subject" name="subject" />
+            </div>
+            <!-- end -->
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Button, FormGroupInput } from "@/components";
+import SendEmailMixin from "@/mixins/SendEmailMixin";
+
+export default {
+  name: "telemarketing-contact",
+  components: {
+    [Button.name]: Button,
+    [FormGroupInput.name]: FormGroupInput
+  },
+  mixins: [SendEmailMixin],
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        message: "",
+        subject: ""
+      }
+    };
+  },
+  computed: {
+    subject() {
+      return `Contact Form Message:  ${this.form.name}`;
+    },
+    sendPlaceholder() {
+      return this.$t("contact.type");
+    },
+    emailPlaceholder() {
+      return this.$t("contact.email");
+    },
+    firstPlaceholder() {
+      return this.$t("contact.name");
+    }
+  },
+  methods: {
+    sendMessage() {
+      this.$swal({
+        title: "Message sent",
+        text: "Our team will review your request and reach out to you shortly.",
+        icon: "success",
+        confirmButtonText: "Create Profile"
+      }).then(result => {
+        if (result.value) {
+          // this.handleEmailNotification();
+          this.form = {};
+          this.$validator.reset();
+          this.$router.push({name: 'landing'})
+        }
+      });
+    },
+    handleEmailNotification() {
+      const email = {};
+      email.message = `<div> Dear ${this.form.name} </div> <div> <p> The Teazeap recruiting team is very happy you have chosen us to help you find the best placement that will suit you. </p> </div> <div> <p> In order for us to fully assist you in the process, we would like to have some relevant information. </p> </div> <ol> <li>For starters, where are you from and where are you currently located?</li> <li>Do you have any teaching experience and are you familiar with teaching kindergarten?</li> <li>Do you have any teaching certificates?</li> <li>Are you fully vaccinated?</li> <li>Finally, When will you be willing to relocate to Taiwan?</li> </ol> <div> <p> Last but not least when will you be available for a quick e-meeting this week? </p> </div> <div> <p> Hope to hear from you soon </p> </div> </div>`;
+      email.subject = "Thanks for reaching out to us";
+      email.address = this.form.email;
+      email.regards = "Regards,";
+      this.sendEmail(email);
+    },
+    handleSubmit() {
+      this.form.subject = this.subject;
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          fetch("/", {
+            method: "post",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: this.encode({
+              "form-name": "telemarketing-contact",
+              ...this.form
+            })
+          }).then(() => {
+            this.sendMessage();
+          });
+        }
+      });
+    },
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.contact-section {
+    padding: 0 0 70px 0;
+    position: relative;
+    background: #FFFFFF;
+    margin: 0 16px;
+}
+</style>
